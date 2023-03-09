e09dea13b570f11575230eb2631dd623b8e8343a
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-03-09 23:10:59 +0300

    Notes Design

--- src/views/ViewNote.vue
+++ src/views/ViewNote.vue
@@ -2,7 +2,19 @@
 
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div v-for="i in 3" class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore ex laboriosam incidunt
+          quo consequuntur quibusdam nisi sit officia dignissimos nulla illum quaerat, maiores
+          corrupti a veritatis quisquam rerum harum consectetur!
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
 
