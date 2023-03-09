be69e831b17a74d92c465537c020337db3913faa
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-03-09 11:01:04 +0300

    Router Add Some Routes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+import ViewNote from '@/views/ViewNote.vue'
+import ViewStats from '@/views/ViewStats.vue'
+
 import App from './App.vue'
 
 import './assets/main.css'
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'notes',
+      component: ViewNote
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: ViewStats
+    }
+  ]
 })
 
 createApp(App).use(router).mount('#app')
