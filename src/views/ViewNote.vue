c5ac8af789fad61e0de627588df3b627dc98a4d9
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-03-09 23:19:06 +0300

    Add Note Form Design

--- src/views/ViewNote.vue
+++ src/views/ViewNote.vue
@@ -2,6 +2,20 @@
 
 <template>
   <div class="notes">
+    <div class="card has-background-success-dark mb-5 p-4">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add new note"></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Add New Note</button>
+        </div>
+      </div>
+    </div>
+
     <div v-for="i in 3" class="card mb-4">
       <div class="card-content">
         <div class="content">
