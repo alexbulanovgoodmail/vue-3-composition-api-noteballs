784b0768981e33865195ca94ca503c516d478f92
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-03-09 11:10:59 +0300

    Router Tidying Up

--- src/main.js
+++ src/main.js
@@ -1,27 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-
-import ViewNote from '@/views/ViewNote.vue'
-import ViewStats from '@/views/ViewStats.vue'
 
 import App from './App.vue'
 
+import router from '@/router'
 // import './assets/main.css'
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'notes',
-      component: ViewNote
-    },
-    {
-      path: '/stats',
-      name: 'stats',
-      component: ViewStats
-    }
-  ]
-})
 
 createApp(App).use(router).mount('#app')
